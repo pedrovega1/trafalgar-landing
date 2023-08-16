ac13e36a6a49993e4ee2daf1f62b064dc760caf0
pedrovega1 <luidzhi.vegas@mail.ru>
2023-08-16 13:56:30 +0600

    //

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import Service from "./components/services/service";
 import Navbar from "./components/navbar/Navbar";
+import Example from "./components/Carousel/Carousel";
 
 function App() {
   return (
     <div className="bg-slate-200 ">
       <Navbar></Navbar>
       <Service></Service>
-
+      {/* <Example></Example> */}
     </div>
   );
 }
